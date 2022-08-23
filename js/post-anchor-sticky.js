404c6d9ecc3beb882924c89c08e2d1c2c706af0a
Asan Azimkulov <azimkulov40@gmail.com>
2022-08-23 13:28:38 +0600

    исправление

--- js/post-anchor-sticky.js
+++ js/post-anchor-sticky.js
@@ -1,7 +1,7 @@
 $(window).scroll(function () {
   const style = window.getComputedStyle(document.querySelector('header'));
   let wrapper = $(".post__anchor__sticky__inner-wrapper");
-  if (Number.parseInt(style.getPropertyValue('--body-padding-height')) === 40) {
+  if (Number.parseInt(style.getPropertyValue('--body-padding-height')) > 0) {
     $(wrapper).addClass('padding-top-100');
   } else {
     $(wrapper).removeClass('padding-top-100');
