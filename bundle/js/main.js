72040e83d3f4edf0c9ccb76f86ef527e39fdb67f
Asan Azimkulov <azimkulov40@gmail.com>
2022-08-23 13:33:34 +0600

    исправление

--- bundle/js/main.js
+++ bundle/js/main.js
@@ -232,7 +232,7 @@ document.querySelectorAll('.image-modal').forEach(modal => modal.addEventListene
 $(window).scroll(function () {
   const style = window.getComputedStyle(document.querySelector('header'));
   let wrapper = $(".post__anchor__sticky__inner-wrapper");
-  if (Number.parseInt(style.getPropertyValue('--body-padding-height')) === 40) {
+  if (Number.parseInt(style.getPropertyValue('--body-padding-height')) > 0) {
     $(wrapper).addClass('padding-top-100');
   } else {
     $(wrapper).removeClass('padding-top-100');
